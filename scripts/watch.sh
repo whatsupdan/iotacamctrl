bf6fd4ab40b018c123535efaa69a915dbcc9ec39
Dan Wood <danielalexwood@gmail.com>
2020-09-19 20:43:35 -0700

    progress...

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -3,7 +3,7 @@
 # get the current path
 CURPATH=$(pwd)
 #--exclude ".*(\.git|\.idea)"
-inotifywait -r -m --exludei ".*(\.git|\.idea|build)" "$CURPATH" |
+inotifywait -r -m --excludei ".*(\.git|\.idea|build)" "$CURPATH" |
    while read path _ file; do
        echo $path$file modified
        make install
