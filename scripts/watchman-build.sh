3d186496a37ba31013da025098b7beab8e236837
Dan Wood <danielalexwood@gmail.com>
2020-09-19 20:10:52 -0700

    progress...

--- scripts/watchman-build.sh
+++ scripts/watchman-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-watchman_build {
+watchman_build() {
 
-  if [[ -z $VIRTUAL_ENV ]]; then
-    if [[ -d "venv" ]]; then
-      source venv/bin/activate
+  if [ -z "$VIRTUAL_ENV" ]; then
+    if [ -d "venv" ]; then
+      . venv/bin/activate
     else
       printf "Error: Unable to determine python virtualenv"
       return
@@ -14,12 +14,8 @@ watchman_build {
     run="make build"
     watchman-make \
         -p 'include/**/*.h' 'lib/*' 'cmake/*.cmake' 'src/*.cpp' 'src/*.h' 'src/**/*.cpp' 'src/**/*.h' \
-        --make=$run
-#        -t "$args"
+        --make="$run"
 }
-
-
-
 
 watchman_build
 
